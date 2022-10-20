1f70c69220485d3f40ae9eef97b6fb439d5b340f
taro <lautaro@airtm.io>
2022-10-20 00:03:27 -0300

    feat: WookieMoviesApiClient (unused and untested yet)

---
+++ src/wookie-movies-api-client.js
@@ -0,0 +1,17 @@
+const apiUrl = 'https://wookie.codesubmit.io/movies'
+
+const createApiUrl = (searchQuery = '') =>
+  !searchQuery
+    ? apiUrl
+    : `${apiUrl}?q=${searchQuery}`
+
+export const WookieMoviesApiClient = ({ authorization = 'Bearer Wookie2021' }) => {
+  const getMovies = (searchQuery = '') =>
+    fetch(createApiUrl(searchQuery), {
+      headers: { authorization },
+    })
+
+  return {
+    getMovies,
+  }
+}
