ab2c1a15e30825532b94bdab0cdaeb6bee937c2b
taro <lautaro@airtm.io>
2022-10-20 06:27:01 -0300

    refactor: MovieCard styled components

--- components/movie-card.tsx
+++ components/movie-card.tsx
@@ -1,23 +1,82 @@
-import { Movie } from '../src/wookie-movies-api-client'
+import styled from 'styled-components'
 
-import styles from '../styles/Home.module.css'
+import { Movie } from '../src/wookie-movies-api-client'
 
 type MovieCardProps = Readonly<{
   movie: Movie
 }>
 
 export const MovieCard = ({ movie }: MovieCardProps) => (
-  <a
+  <MovieCardStyled
     key={movie.id}
     href={`/movies/${movie.slug}`}
-    className={styles.card}
   >
-    <div className={styles.cardBackground} style={{ backgroundImage: `url(${movie.backdrop})` }}></div>
+    <MovieCardBackground style={{ backgroundImage: `url(${movie.backdrop})` }} />
     <h2>{movie.title}</h2>
-    <div className={styles.cardInfo}>
+    <MovieCardInfo>
       <span>⭐ {movie.imdb_rating}</span>
       <span>{movie.length}</span>
-    </div>
+    </MovieCardInfo>
     <p>{movie.overview}</p>
-  </a>
+  </MovieCardStyled>
 )
+
+const MovieCardStyled = styled.a`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 370px;
+  height: 270px;
+  margin: 1rem;
+  padding: 0;
+  text-align: left;
+  color: white;
+  text-shadow: 0 0 10px black;
+  text-decoration: none;
+  border-radius: 10px;
+  transition: color 0.15s ease, border-color 0.15s ease, box-shadow 0.15s ease;
+  overflow: hidden;
+
+  &:hover,
+  &:focus,
+  &:active {
+    box-shadow: 0 0 2px 2px #000000aa, 0 0 2px 1px #000000aa inset;
+    
+    & h2 {
+      text-decoration: underline;
+    }
+  }
+  
+  h2 {
+    margin: 0;
+    font-size: 1.5rem;
+    padding: 1.5rem;
+    backdrop-filter: saturate(0.5) brightness(0.5);
+  }
+  
+  p {
+    margin: 1rem 1.5rem 1.5rem 1.5rem;;
+    font-size: 1.25rem;
+    line-height: 1.5;
+    overflow: hidden;
+  }
+`
+
+const MovieCardBackground = styled.div`
+  z-index: -1;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  filter: blur(3px);
+  background-size: cover;
+`
+
+const MovieCardInfo = styled.div`
+  margin: 1.5rem 1.5rem 0 1.5rem;
+  font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
