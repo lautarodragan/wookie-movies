6a4628e63544e8955d8d4a0dcc5c5a3c64eed95a
taro <lautaro@airtm.io>
2022-10-23 01:22:06 -0300

    test: setup + 1 pure test

---
+++ __tests__/pure/group-movies-by-genre.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from '@jest/globals';
+
+import { groupMoviesByGenre } from '../../pure/group-movies-by-genre'
+import movies from './input.json'
+import output from './output.json'
+
+describe('groupMoviesByGenre', () => {
+  test('work as expected on the common API response', () => {
+    expect(groupMoviesByGenre(movies)).toEqual(output);
+  });
+})
